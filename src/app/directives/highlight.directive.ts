56a11c687d97fc285552cbe840d806c46394fc64
Jeff Wu <crazy.alu@outlook.com>
2017-04-08 11:47:38 +0800

    correct the selector name and apply highlight directive.

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, OnInit } from '@angular/core';
 
 @Directive({
-  selector: '[appHilight]'
+  selector: '[appHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit {
 
-  constructor() { }
+  constructor(private _el: ElementRef) {
+    // 注入 elementRef 就可以取得 DOM
+    console.log('directive init,', this._el);
+  }
+
+  ngOnInit() {
+    // 設定 CSS 樣式
+    this._el.nativeElement.style.backgroundColor = 'orange';
+    this._el.nativeElement.style.color = 'white';
+    this._el.nativeElement.style.display = 'inline-block';
+    this._el.nativeElement.style.width = '120px';
+    this._el.nativeElement.style.height = '40px';
+    this._el.nativeElement.style.textAlign = 'center';
+    this._el.nativeElement.style.lineHeight = '2.5';
+  }
 
 }
